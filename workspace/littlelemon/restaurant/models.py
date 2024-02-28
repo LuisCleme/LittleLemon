dee014effe2279d4b6fec6914092d98b56a4b9a4
Luis Clemente <luisclemegmz@gmail.com>
2024-02-28 09:28:37 -0600

    hola

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    """Model definition for Booking."""
+
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        """Unicode representation of Booking."""
+        pass
+
+class MenuItem(models.Model):
+    """Model definition for Menu."""
+
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
+    
+    def __str__(self):
+        """Unicode representation of Menu."""
+        pass
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from rest_framework import viewsets
+from .serializers import MenuItemSerializer, BookingSerializers
+from .models import MenuItem, Booking
 # Create your views here.
 
 def index (request):
     return render(request, "index.html", {}) 
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializers
